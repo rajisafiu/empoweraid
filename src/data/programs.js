99d50456a13f22fb294eb56c9d0a43b0b78daed3
rajisafiu <rajikolawole677@gmail.com>
2026-03-02 18:11:10 +0100

    update

--- src/data/programs.js
+++ src/data/programs.js
@@ -3,17 +3,17 @@
 export const programs = [
   {
     tittle: "Small Business Support",
-    amount: "Up to $100,000",
+    amount: "Up to $250,000",
     description: "Funding support for entrepreneurs and small Business."
   },
   {
     tittle: "Retiree Assiatance",
-    amount: "$50,000",
+    amount: "$150,000",
     description: "Support for retired individuals and senior citizens.",
   },
   {
     tittle: "Youth Startup Funds",
-    amount: "$30,000",
+    amount: "$50,000",
     description: "Helping young innovators launch new ventures"
   }
   
